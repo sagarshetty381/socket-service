d654062a04d7cd20e644b771689e4d1afba9b5ff
Sagar Shetty <37378945+sagarshetty381@users.noreply.github.com>
2024-07-06 13:36:32 +0530

    added build command and code fixes

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import SocketService from "./src/services/socket.connection";
 import http from "http";
 import * as config from './config.json';
 import { startMessageConsumer } from './src/services/kafka';
+import path from 'path';
 
 const cors = require('cors');
 const router = express.Router();
@@ -12,37 +13,34 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const modulesPath = './src';
+const modulesPath = path.join(__dirname, './src')
 const apiBase = '/master/api';
 
-initRoutes(modulesPath);
 app.use(router);
-
-app.listen(config.masterPort, () => console.log(`Socket Server is running on ${config.masterPort}`));
 
 function initService() {
     // startMessageConsumer();
-    const PORT = process.env.PORT ? process.env.PORT : config.socketPort;
-    const httpServer = http.createServer();
-
+    initRoutes(modulesPath);
+    const PORT = process.env.PORT ? process.env.PORT : config.port;
     const socketService = new SocketService();
+    const httpServer = http.createServer(app);
     socketService.io.attach(httpServer);
-    httpServer.listen(PORT, () =>
+    
+    app.listen(PORT, () =>
         console.log(`HTTP Server started at PORT:${PORT}`)
     );
 
     socketService.initListeners();
-}
+}  
 
 function initRoutes(filePath: string) {
     const folders = fs.readdirSync(filePath);
     folders.forEach(file => {
         const fullName = `${filePath}/${file}`;
         const stat = fs.lstatSync(fullName);
-        const routeFilePath = fs.existsSync(`${fullName}/${file}.route.ts`);
-
+        const routeFilePath = fs.existsSync(process.env.NODE_ENV == 'prod' ? `${fullName}/${file}.route.js` : `${fullName}/${file}.route.ts`);
         if (stat.isDirectory() && routeFilePath) {
-            const router = require(`${fullName}/${file}.route.ts`).default;
+            const router = require(process.env.NODE_ENV == 'prod' ? `${fullName}/${file}.route.js` : `${fullName}/${file}.route.ts`).default;
             app.use(apiBase, router);
         } else if (stat.isDirectory()) {
             initRoutes(fullName);
